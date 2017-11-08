25a18a3a811664ed64fd83d80148b76dff139416
SebastiaN <odm_sd@hotmail.com>
2017-11-08 16:13:51 -0500

    apli de registro completa

--- app/Http/Controllers/AmbatoController.php
+++ app/Http/Controllers/AmbatoController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Ambato\Carrera;
 use App\Ambato\Estudiante;
+use App\Ambato\Facultad;
 use App\Carreras;
 use App\Facultades;
 use Illuminate\Http\Request;
@@ -26,13 +28,16 @@ class AmbatoController extends Controller{
     }
 
     public function show($cedula){
-        $consulta   =   Estudiante::where('ci_a',$cedula)->get();
+        $consulta   =   Estudiante::select('carreras_a.id_ca as carrera','nombre_ca as carreran','nombres_a as nombres','celular_a as celular','telefono_a as telefono','sexo_a as sexo','usuario_a as modalidad','porcentaje_a as porcentaje','ci_a as cedula','expediente_a as expediente','nivel_a as nivel','id_fa as facultad','pregrado_ca as pregrado')
+                                    ->join('carreras_a','carreras_a.id_ca','=','ambato.id_ca')
+                                    ->where('ci_a',$cedula)->get();
         return $consulta;
     }
 
 
 
     public function actualizar(){
+        ini_set('max_execution_time', 120);
         $this->soapWrapper->add('Currency', function ($service) {
             $service
                 ->wsdl('http://app.pucesa.edu.ec:9080/ServiciosAcademics/wsBolsaTrabajoQuito.asmx?WSDL')
@@ -47,42 +52,68 @@ class AmbatoController extends Controller{
         foreach ($response as $item){
             $a['cedula']        =   $item->cedula;
             $a['anio']          =   $item->anio;
-            $a['nombres']       =   $item->nombre;
+            $a['nombres']       =   mb_convert_case($item->nombre, MB_CASE_TITLE,'UTF-8');
             $a['porcentaje']    =   (int)$item->porcentaje;
-            $a['carrera']       =   $item->carrera;
-            $a['carrera']       =   $item->carrera;
+            $a['carrera']       =   $this->codigoCarrera($item->carrera,$item->facultad,$item->preposgrado);
+            $a['carreran']      =   $item->carrera;
             $a['facultad']      =   $item->facultad;
             $a['nacimiento']    =   $item->nacimiento;
             $a['telefono']      =   ($item->telefono==="NN"?null:$item->telefono);
             $a['celular']       =   ($item->celular==="NN"?null:$item->celular);
             $a['sexo']          =   ($item->sexo==='F'?0:1);
-            $a['plan']          =   $item->plan;
-            $a['expediente']    =   $item->expediente;
+            $a['plan']          =   ($item->plan===''?null:$item->plan);
+            $a['expediente']    =   ($item->expediente===''?null:$item->expediente);
             $a['impedimento']   =   ($item->impedimento==="N"?false:true);
             $a['nivel']         =   $item->nivel;
             $a['modalidad']     =   $item->modalidad;
             $a['pregrado']      =   ($item->preposgrado==="PREGRADO"?true:false);
             $a['actualizacion'] =   ($item->actualizacion==="TRUE"?true:false);
+
+            $estudiante =   Estudiante::where('ci_a',$a['cedula'])->where('id_ca',$a['carrera'])->first();
+            if(!$estudiante){
+                $estudiante =   new Estudiante();
+                $estudiante->id_ca          =   $a['carrera'];
+                $estudiante->ci_a           =   $a['cedula'];
+                $estudiante->nombres_a      =   $a['nombres'];
+                $estudiante->celular_a      =   $a['celular'];
+                $estudiante->telefono_a     =   $a['telefono'];
+                $estudiante->sexo_a         =   $a['sexo'];
+                $estudiante->porcentaje_a   =   $a['porcentaje'];
+                $estudiante->plan_a         =   $a['plan'];
+                $estudiante->expediente_a   =   $a['expediente'];
+                $estudiante->nivel_a        =   $a['nivel'];
+                $estudiante->usuario_a      =   $a['modalidad'];//no hay un campo para modalidad
+                $estudiante->save();
+            }else{
+                Estudiante::where('ci_a',$a['cedula'])->where('id_ca',$a['carrera'])->update(['porcentaje_a'=>$a['porcentaje'],'nivel_a'=>$a['nivel']]);
+            }
             $aux[]=$a;
         }
         return $aux;
     }
-    public function codigoCarrera($carrera,$facultad){
-        $carrera=ucfirst($carrera);
+    public function codigoCarrera($carrera,$facultad,$tipo){
+        $carrera=mb_convert_case($carrera, MB_CASE_TITLE);
         if($carrera){
             $carreras   =   Carreras::join('facultades','escuelas.ID_FAC','=','facultades.ID_FAC')->where('NOMBRE_ESC',$carrera)->where('ID_UNIVERSIDAD',$this->universidad)->first();
             if(!$carreras){
-                $carreras   = new Carreras();
-                $carreras->NOMBRE_ESC   =   $carreras;
-                $carreras->ID_FAC       = $this->codigoFacultad($facultad);
+                $carreras               =   new Carreras();
+                $carreras->NOMBRE_ESC   =   $carrera;
+                $carreras->ID_FAC       =   $this->codigoFacultad($facultad);
+                $carreras->TIPO_ESC     =   $tipo;
                 $carreras->save();
+
+                $carinterna             =   new Carrera();
+                $carinterna->id_fa      =   $carreras->ID_FAC;
+                $carinterna->id_ca      =   $carreras->CODIGO_ESC;
+                $carinterna->nombre_ca  =   $carrera;
+                $carinterna->save();
             }
             return $carreras->CODIGO_ESC;
         }
         return false;
     }
     public function codigoFacultad($facultad){
-        $facultad=ucfirst($facultad);
+        $facultad=mb_convert_case($facultad, MB_CASE_TITLE);
         if($facultad){
             $facultades =   Facultades::where('ID_UNIVERSIDAD',$this->universidad)->where('NOMBRE_FAC',$facultad)->first();
             if(!$facultades){
@@ -90,6 +121,11 @@ class AmbatoController extends Controller{
                 $facultades->NOMBRE_FAC     =   $facultad;
                 $facultades->ID_UNIVERSIDAD =   $this->universidad;
                 $facultades->save();
+
+                $facinterna                 =   new Facultad();
+                $facinterna->id_fa          =   $facultades->ID_FAC;
+                $facinterna->nombre_fa      =   $facultad;
+                $facinterna->save();
             }
             return $facultades->ID_FAC;
         }
