ad1f2485d7ab62efa27a05e38c816e4ba1f692d6
Sebastian <sebastian.robalino@inpsercom.com>
2020-08-23 18:40:27 -0500

    login integrado

--- routes/web.php
+++ routes/web.php
@@ -10,10 +10,12 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+Auth::routes();
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/home', 'HomeController@index')->name('home');
 Route::get('dni',"ConsultaInterface@index")->name('consulta');
 Route::get('dni/{dni}',"ConsultaInterface@show");
 
